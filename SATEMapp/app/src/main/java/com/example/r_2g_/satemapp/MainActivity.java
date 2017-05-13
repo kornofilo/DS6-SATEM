0fe144c887b7112e9bd3d8ccda8bdf7fbef2ad2e
Logan8192 <wsses11000@gmail.com>
2017-05-13 00:36:40 -0500

    SATEM WEB - Footer Added.

--- SATEMapp/app/src/main/java/com/example/r_2g_/satemapp/MainActivity.java
+++ SATEMapp/app/src/main/java/com/example/r_2g_/satemapp/MainActivity.java
@@ -1,3 +1,10 @@
+/*
+        Desarrollo De Software VI
+        Proyecto Final - SATEM
+        Elaborado por: Aldair de Gracia, Ricardo Rubio, Víctor Pineda
+        Archivo:  (MainActivity.java)
+*/
+
 package com.example.r_2g_.satemapp;
 
 import android.content.Intent;

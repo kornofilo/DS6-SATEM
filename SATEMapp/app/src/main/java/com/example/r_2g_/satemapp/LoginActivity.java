d6ad91fcca73c18455946e1069f350e5a53cab6b
Logan8192 <wsses11000@gmail.com>
2017-05-18 18:44:19 -0500

    SATEM APP - Activities Update 2

--- SATEMapp/app/src/main/java/com/example/r_2g_/satemapp/LoginActivity.java
+++ SATEMapp/app/src/main/java/com/example/r_2g_/satemapp/LoginActivity.java
@@ -1,13 +1,27 @@
 package com.example.r_2g_.satemapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent;
+        switch (v.getId()){
+            case R.id.buttonLogin:{
+                intent = new Intent(LoginActivity.this,MainActivity.class);
+                startActivity(intent);
+                break;
+            }
+        }
     }
 }

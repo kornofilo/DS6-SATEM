48f29dbb7f5a694dcb77f1393e92029e05a4db53
aldair1de1gracia <aldair774@hotmail.com>
2017-06-09 16:45:24 -0500

    recuperar pacientes
    first

---
+++ Satem Web/WebPage/getpaciente.js
@@ -0,0 +1,32 @@
+/*  
+    Desarrollo De Software VI
+    Proyecto Final - SATEM
+    Elaborado por: Aldair de Gracia, Ricardo Rubio, Víctor Pineda 
+    Archivo:  (getData.js)
+*/
+
+//Obtenemos las emergencias Registradas.
+var tableAsignar = document.getElementById('tabla_paciente');
+        
+
+          
+
+    //Cargamos los datos de la DB de Firebase.
+    var dbRefObject = firebase.database().ref('pacientes/');
+    var contAsignar = 0;
+    
+    dbRefObject.orderByKey().equalTo("12346").on("child_added", function(data) {
+        contAsignar += 1;
+        console.log(data);
+
+        var row = tableAsignar.insertRow(-1);
+        var cell1 = row.insertCell(0); 
+        cell1.innerHTML = data.key;
+        var cell2 = row.insertCell(1);
+        cell2.innerHTML = data.val().nombre;
+        var cell2 = row.insertCell(2);
+        cell3.innerHTML = data.key;
+        var cell3 = row.insertCell(3);
+       
+});
+

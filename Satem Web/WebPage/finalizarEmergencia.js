1bfe67aaaf6f63ae212da64d5721026db6a0c6b0
Logan8192 <wsses11000@gmail.com>
2017-06-20 13:45:36 -0500

    SATEM WEB - FINALIZE EMERGENCY 3

--- Satem Web/WebPage/finalizarEmergencia.js
+++ Satem Web/WebPage/finalizarEmergencia.js
@@ -9,23 +9,33 @@
     function finalizarEmergencia(element) {
 
     		var table = document.getElementById('enCaminoTable');
-     	  	console.log(element);
      	  	var button = document.getElementById(element.id);
      	  	var rowvalue = button.parentNode.parentNode.rowIndex;
      	  	var cell = table.rows[rowvalue].cells[0];
      	  	var idEmergencia = cell.firstChild.data;
-        	console.log(idEmergencia);
 
 
 
         	$('ul.tabs').tabs('select_tab', 'test4');
 
+            //Seteamos el estado de la emergencia seleccionada como Finalizada.
         	var dbRefEmergencia = firebase.database().ref('emergencias/' + idEmergencia + '/estado');
-        	console.log(dbRefEmergencia);
         	dbRefEmergencia.transaction(function(status) {
         		status = "Finalizada";
    				return status;
 			});
+
+
+            dbRefEmergencia = firebase.database().ref('emergencias/' + idEmergencia + '/ambulancia');
+            //Seteamos a las ambulancias asignadas a la emergencia seleccionada como Disponibles.
+            dbRefEmergencia.on("child_added", function(data) {
+
+               var dbRefAmbulancia = firebase.database().ref('ambulancias/' + data.key + '/estado' );
+               dbRefAmbulancia.transaction(function(status) {
+                    status = "Disponible";
+                    return status;
+                });
+             });
 
          
    }

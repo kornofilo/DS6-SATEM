578816286f7c59a4cd2b932e5c2f7aee79a595a4
aldair1de1gracia <aldair774@hotmail.com>
2017-06-22 19:48:09 -0500

    comit
    hola amigos

--- Satem Web/WebPage/java.js
+++ Satem Web/WebPage/java.js
@@ -2,24 +2,38 @@ window.onload =inicializar;
 
 var refambulancias;
 var tbodytablaambulancias;
+var ambulancias=[];
 function inicializar() {
  tbodytablaambulancias=document.getElementById("tabla-ambulancias"); 
     refambulancias=firebase.database().ref().child("ambulancias");
-    mostrarambulancias();
+
+    refambulancias.on("value",function(snap){
+    var datos=snap.val();
+    console.log(datos);
+    var x=0;
+    for (var key in datos) {
+        ambulancias[x]= parseInt(datos[key].cantidadEmergencias);
+        x++;               
+    }
+    });
+  console.log(ambulancias);
+  var barChartData = {
+    labels : ["Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio"],
+    datasets : [
+      {
+        fillColor : "#6b9dfa",
+        strokeColor : "#ffffff",
+        highlightFill: "#1864f2",
+        highlightStroke: "#ffffff",
+        data : ambulancias      }
+    ]
+
+  } 
+    
+  var ctx3 = document.getElementById("chart-area3").getContext("2d");
+  window.myPie = new Chart(ctx3).Bar(barChartData, {responsive:true});
+      
  } 
 
 
-  function mostrarambulancias() {
-  	refambulancias.on("value",function(snap){
-    var datos=snap.val();
-    var filasamostrar="";
-    for (var key in datos) {
-    	filasamostrar+="<tr>"+
-	    	               "<td>"+ datos[key].cantidadEmergencias  +"</td>"+
-	    	               "<td>"+ datos[key].emergenciaActual  +"</td>"+
-	    	               "<td>"+ datos[key].estado  +"</td>"+
-                       "</tr>";
-      }
-      tbodytablaambulancias.innerHTML= filasamostrar;
-  	});
-  }
+  

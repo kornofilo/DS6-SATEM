2461ac3987e7c874a1373baaedee9452287bac3c
Logan8192 <wsses11000@gmail.com>
2017-06-26 01:01:11 -0500

    SATEM WEB - IMPROVING CODE 3-1

--- Satem Web/WebPage/getData.js
+++ Satem Web/WebPage/getData.js
@@ -131,19 +131,16 @@ $(document).ready(function(){
           $('.dataTables_filter').remove();
 
 
-          dbRefObjectPacientes.on("child_added", function(data) {
-          var dataSet = [data.val().nombre,data.val().cedula,data.val().genero,data.val().numAmbulancia,data.val().lugarAccidente,data.val().suceso,data.val().fecha,data.val().sintomas,data.val().diagnostico,data.val().condicionVital,data.val().riesgo];
-          
-          tablePacientes.node().id = "1";
-          tablePacientes.
-            rows.
-            add([dataSet]).
-            draw();        
-          });
+          dbRefObjectPacientes.on('child_added', function(data) {    
+           var rowIndex = $('#pacientesTable').dataTable().fnAddData([data.val().nombre,data.val().cedula,data.val().genero,data.val().numAmbulancia,data.val().lugarAccidente,data.val().suceso,data.val().fecha,data.val().sintomas,data.val().diagnostico,data.val().condicionVital,data.val().riesgo]);
+           var row = $('#pacientesTable').dataTable().fnGetNodes(rowIndex);
+           $(row).attr('id', data.key);
+        });
 
-          dbRefObjectPacientes.on("child_changed", function(data) {
-            //         
-          });
+
+       dbRefObjectPacientes.on("child_changed", function(data) {
+           $('#pacientesTable').dataTable().fnUpdate( [data.val().nombre,data.val().cedula,data.val().genero,data.val().numAmbulancia,data.val().lugarAccidente,data.val().suceso,data.val().fecha,data.val().sintomas,data.val().diagnostico,data.val().condicionVital,data.val().riesgo], document.getElementById(data.key), undefined);
+        });
 
            //Seteamos el searchbar superior para que pueda realizar un filtrado de la tabla.
            $('#cedulaInput').keyup(function(){

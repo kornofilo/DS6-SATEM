54833679223cdc584aeb9c117ae478d33e838c22
Logan8192 <wsses11000@gmail.com>
2017-06-28 15:45:57 -0500

    SATEM WEB - CHART BUG FIXED (KINDA)

---
+++ Satem Web/WebPage/analytics.js
@@ -0,0 +1,50 @@
+window.onload =inicializar;
+
+
+function inicializar() {
+  var refambulancias;
+  var tbodytablaambulancias;
+  var ambulancias=[];
+  var keyAmbulancias = [];
+  var barChartData = {};
+    refambulancias=firebase.database().ref('ambulancias/');
+
+    refambulancias.on('child_added', function(snapshot) {
+        keyAmbulancias.push(snapshot.key);
+        ambulancias.push(snapshot.val().cantidadEmergencias);
+         
+      // Dibujamos el Chart
+      new Chart(document.getElementById("chart-ambulancias"), {
+          type: 'bar',
+          data: {
+            labels: keyAmbulancias,
+            datasets: [
+            {
+              backgroundColor: "#FF8C00",
+              label: "Cantidad de Emergencias",
+              data: ambulancias
+            }
+          ]
+    },
+    options: {
+          events: ['click'],
+          title: {
+        display: true,
+        text: 'Cantidad de Emergencias por Ambulancia'
+        }
+      }
+    });
+    });
+
+
+
+
+
+   
+  
+ 
+      
+ } 
+
+
+  
